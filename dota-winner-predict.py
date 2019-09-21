7d3d1c9542bb0e65f8ed894706721dd4fcb8c9bf
Dmitrii Metelkov <49140856+ioniffer@users.noreply.github.com>
2019-09-21 21:51:40 +0400

    Add files via upload

--- dota-winner-predict.py
+++ dota-winner-predict.py
@@ -1,5 +1,6 @@
 import pandas
 import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import roc_auc_score
 
 data = pandas.read_csv('features.csv', index_col='match_id')
@@ -8,10 +9,20 @@ y_train = data['radiant_win']
 
 nan_count = X_train.isnull().sum()
 nan_count = nan_count[nan_count != 0]
+print('Признаки с пропусками (NaN) |  Количество пропусков')
 print(nan_count)
+
+X_train.fillna(inplace=True, value=0)
+
+print('Столбец с целевой переменной - radiant_win')
 
 data_test = pandas.read_csv('features_test.csv', index_col='match_id')
 X_test = data_test.loc[:, 'start_time':'dire_first_ward_time']
+X_test.fillna(inplace=True, value=0)
+
+gbm = GradientBoostingClassifier(n_estimators=250, verbose=True,
+                                 random_state=241, learning_rate=lr)
+gbm.fit(X_train, y_train)
 
 # print(X_test)
 
